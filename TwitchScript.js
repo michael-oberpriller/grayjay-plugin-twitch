62b274776643f2b4dda59083d60ba54274a636e0
michael-oberpriller <93411352+michael-oberpriller@users.noreply.github.com>
2024-11-24 00:07:06 +0000

    Update TwitchScript.js
    Signed-off-by: michael-oberpriller <93411352+michael-oberpriller@users.noreply.github.com>

--- TwitchScript.js
+++ TwitchScript.js
@@ -384,7 +384,7 @@ function getLiveVideo(url, video_details = true) {
 
     const spat = playback_access_token.data.streamPlaybackAccessToken
 
-    const hls_url = `https://api.ttv.lol/api/channel/hls/${login}.m3u8?acmb=e30=&allow_source=true&fast_bread=true&p=&play_session_id=&player_backend=mediaplayer&playlist_include_framerate=true&reassignments_supported=true&sig=${spat.signature}&supported_codecs=avc1&token=${encodeURIComponent(spat.value)}&transcode_mode=vbr_v1&cdm=wv&player_version=1.20.0`
+    const hls_url = `https://lb-eu.cdn-perfprod.com/live/${login}.m3u8?acmb=e30=&allow_source=true&fast_bread=true&p=&play_session_id=&player_backend=mediaplayer&playlist_include_framerate=true&reassignments_supported=true&sig=${spat.signature}&supported_codecs=avc1&token=${encodeURIComponent(spat.value)}&transcode_mode=vbr_v1&cdm=wv&player_version=1.20.0`
 
     checkHLS(hls_url)
 
