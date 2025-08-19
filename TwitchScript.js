b39aabaf0f02c1e60996acee106215f20a3e103f
Michael Oberpriller <michael.oberpriller@protonmail.com>
2025-08-18 20:51:31 -0500

    Test

--- TwitchScript.js
+++ TwitchScript.js
@@ -489,7 +489,7 @@ function getLiveVideo(url, video_details = true) {
 
     const spat = playback_access_token.data.streamPlaybackAccessToken
 
-    const hls_url = (_settings.useProxy === true ? `http://firefox.api.cdn-perfprod.com` : `https://usher.ttvnw.net`) + `/api/channel/hls/${login}.m3u8?acmb=e30=&allow_source=true&fast_bread=true&p=&play_session_id=&player_backend=mediaplayer&playlist_include_framerate=true&reassignments_supported=true&sig=${spat.signature}&supported_codecs=avc1&token=${encodeURIComponent(spat.value)}&transcode_mode=vbr_v1&cdm=wv&player_version=1.20.0`
+    //const hls_url = (_settings.useProxy === true ? `http://firefox.api.cdn-perfprod.com` : `https://usher.ttvnw.net`) + `/api/channel/hls/${login}.m3u8?acmb=e30=&allow_source=true&fast_bread=true&p=&play_session_id=&player_backend=mediaplayer&playlist_include_framerate=true&reassignments_supported=true&sig=${spat.signature}&supported_codecs=avc1&token=${encodeURIComponent(spat.value)}&transcode_mode=vbr_v1&cdm=wv&player_version=1.20.0`
 
     const hls_url = `http://firefox.api.cdn-perfprod.com/api/channel/hls/${login}.m3u8?acmb=e30=&allow_source=true&fast_bread=true&p=&play_session_id=&player_backend=mediaplayer&playlist_include_framerate=true&reassignments_supported=true&sig=${spat.signature}&supported_codecs=avc1&token=${encodeURIComponent(spat.value)}&transcode_mode=vbr_v1&cdm=wv&player_version=1.20.0`
     bridge.toast(hls_url)
