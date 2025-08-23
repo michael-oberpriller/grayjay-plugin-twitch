d47800ead8b1e120daf9cb3a6e7c3379035d700c
Michael Oberpriller <michael.oberpriller@protonmail.com>
2025-08-23 17:55:43 -0500

    Change url

--- TwitchScript.js
+++ TwitchScript.js
@@ -347,10 +347,8 @@ function getSavedVideo(url) {
 
     const spat = hls_json.data.videoPlaybackAccessToken
 
-    const hls_url = (_settings.useProxy === true ? `https://lb-eu.cdn-perfprod.com` : `https://usher.ttvnw.net`) + `/vod/${id}.m3u8?acmb=e30=&allow_source=true&fast_bread=true&p=&play_session_id=&player_backend=mediaplayer&playlist_include_framerate=true&reassignments_supported=true&sig=${spat.signature}&supported_codecs=avc1&token=${encodeURIComponent(spat.value)}&transcode_mode=vbr_v1&cdm=wv&player_version=1.20.0`
+    const hls_url = `https://usher.ttvnw.net/vod/${id}.m3u8?acmb=e30=&allow_source=true&fast_bread=true&p=&play_session_id=&player_backend=mediaplayer&playlist_include_framerate=true&reassignments_supported=true&sig=${spat.signature}&supported_codecs=avc1&token=${encodeURIComponent(spat.value)}&transcode_mode=vbr_v1&cdm=wv&player_version=1.20.0`
 	
-    bridge.toast(hls_url)
-
     checkHLS(hls_url)
 
     const sources = [new HLSSource({ name: 'source', duration: 0, url: hls_url })]
@@ -489,7 +487,7 @@ function getLiveVideo(url, video_details = true) {
 
     const spat = playback_access_token.data.streamPlaybackAccessToken
 
-    const hls_url = _settings.useProxy === true ? `https://lb-eu.cdn-perfprod.com/playlist/${login}` : `https://usher.ttvnw.net/api/channel/hls/${login}.m3u8?acmb=e30=&allow_source=true&fast_bread=true&p=&play_session_id=&player_backend=mediaplayer&playlist_include_framerate=true&reassignments_supported=true&sig=${spat.signature}&supported_codecs=avc1&token=${encodeURIComponent(spat.value)}&transcode_mode=vbr_v1&cdm=wv&player_version=1.20.0`
+    const hls_url = _settings.useProxy === true ? `https://eu2.luminous.dev/live/${login}` : `https://usher.ttvnw.net/api/channel/hls/${login}.m3u8?acmb=e30=&allow_source=true&fast_bread=true&p=&play_session_id=&player_backend=mediaplayer&playlist_include_framerate=true&reassignments_supported=true&sig=${spat.signature}&supported_codecs=avc1&token=${encodeURIComponent(spat.value)}&transcode_mode=vbr_v1&cdm=wv&player_version=1.20.0`
 
     bridge.toast(hls_url)
     
